aa84a49a918378cad24e81c1f34eccf4ecb705ab
Frederico <fredlgprime@gmail.com>
2020-08-12 10:01:23 -0300

    Adição das funções de ADIÇÃO, SUBTRAÇÃO, MULTIPLICAÇÃO e DIVISÃO. Implementação das views SOMA, MULTIPLICACAO, DIVISAO E SUBTRACAO com passagem de parâmetro.

--- core/views.py
+++ core/views.py
@@ -3,5 +3,30 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 
-def hello(request, nome):
-    return HttpResponse(f'<h1>Hello {nome}<h1>')
+def hello(request, nome, idade):
+    return HttpResponse(f'<h1>Hello {nome} tem {idade} anos.<h1>')
+
+
+def soma(request, v1, v2):
+    soma = v1 + v2
+    return HttpResponse(f'<h2>A soma entre {v1} e {v2} é igual a {soma}<h2>')
+
+
+def multiplicacao(request, v1, v2):
+    mult = v1 * v2
+    return HttpResponse(f'<h2>O produto entre {v1} e {v2} é igual a {mult}<h2>')
+
+
+def divisao(request, v1, v2):
+    try:
+        div = v1 / v2
+        return HttpResponse(f'<h2>A divisão entre {v1} e {v2} é igual a {div}<h2>')
+    except Exception as e:
+        return HttpResponse(f'Por favor, procurar o administrado do sistema. ERRO: {e}')
+    except ZeroDivisionError:
+        return HttpResponse('<h1>Dvisão por ZERO não existe!<h1>')
+
+
+def subtracao(request, v1, v2):
+    sub = v1 - v2
+    return HttpResponse(f'<h2>A subtração entre {v1} e {v2} é igual a {sub}<h2>')
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -19,5 +19,9 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/', views.hello)
+    path('hello/<nome>/<int:idade>/', views.hello),
+    path('soma/<int:v1>/<int:v2>/', views.soma),
+    path('multiplicacao/<int:v1>/<int:v2>/', views.multiplicacao),
+    path('divisao/<int:v1>/<int:v2>/', views.divisao),
+    path('subtracao/<int:v1>/<int:v2>/', views.subtracao)
 ]
